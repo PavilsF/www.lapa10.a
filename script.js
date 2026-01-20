34d63770c4f1fbf83f2e53bb44845cf175b5698f
PavilsF <pavils.florens@edu.kekava.lv>
2026-01-20 10:24:35 +0200

    sports ir pabeigts

---
+++ script.js
@@ -0,0 +1,15 @@
+function poga(){
+    window.location.href="basketbols.html"
+}
+
+function poga1(){
+    window.location.href="Krikets.html"
+}
+
+function poga2(){
+    window.location.href="Futbols.html"
+}
+
+function poga3(){
+    window.location.href="sports.html"
+}
